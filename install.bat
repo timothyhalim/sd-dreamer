1fe60e211519d35bc656536e318a94c1a0234b1b
Timothy Halim <timothy.halim@live.com>
2022-09-25 10:00:52 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,24 @@
+ECHO Setting Current Directory
 %~d0
-cd %~dp0
+cd "%~dp0"
 
+ECHO Initializing Submodule
 git submodule init
 git submodule update --remote
 
+ECHO Creating Environment Variable
 python -m venv venv
 
-call %~dp0/venv/Scripts/activate.bat
+ECHO Activating Environment Variable
+call "%~dp0/venv/Scripts/activate.bat"
 
+ECHO Installing Dependencies
 python dreamer/install_dependencies.py
 python -m pip install -r requirements.txt
 
+ECHO Preparing Weight
+rename "%~dp0/sd-v1-4.ckpt" "%~dp0/model.ckpt"
+move "%~dp0/model.ckpt" "%~dp0/dreamer/stable_diffusion/models/ldm/stable-diffusion-v1"
+
+ECHO Install Complete
 start run.bat
